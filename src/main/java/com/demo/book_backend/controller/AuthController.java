ded74a1d694037a3c57a09209ec696aa3a04d944
atikahadzman <atikah.adzman@gmail.com>
2026-01-11 16:43:23 +0800

    fix cross origin issue

--- src/main/java/com/demo/book_backend/controller/AuthController.java
+++ src/main/java/com/demo/book_backend/controller/AuthController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import com.demo.book_backend.dto.LoginRequest;
 import com.demo.book_backend.model.Users;
 import com.demo.book_backend.repository.UserRepository;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
