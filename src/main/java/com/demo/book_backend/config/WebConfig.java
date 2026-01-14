25310c5b32557d02f52002eec479fa89f67e697a
atikahadzman <atikah.adzman@gmail.com>
2026-01-14 17:26:37 +0800

    fixed pdf file not working

--- src/main/java/com/demo/book_backend/config/WebConfig.java
+++ src/main/java/com/demo/book_backend/config/WebConfig.java
@@ -23,10 +23,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry
-            .addMapping("/uploads/**")
+        registry.addMapping("/**")
             .allowedOrigins("http://localhost:4200")
-            .allowedMethods("GET")
-            .allowedHeaders("*");
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
     }
 }
